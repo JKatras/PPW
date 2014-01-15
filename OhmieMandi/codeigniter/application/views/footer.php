ed2017b8ebee0f1aa2cc3b07b864c673635b9063
Jeremy Katras <jkatras@fullsail.edu>
2014-01-15 11:42:54 -0500

    added google analytics tracker to footer

--- OhmieMandi/codeigniter/application/views/footer.php
+++ OhmieMandi/codeigniter/application/views/footer.php
@@ -23,9 +23,23 @@ echo "<a href='index.php?action=login'>User Login</a>
 	<script src="<?php echo base_url(); ?>public/js/vendor/custom.modernizr.js"></script>
 	<script src="<?php echo base_url(); ?>public/js/vendor/jquery.js" type="text/javascript"></script>
 	<script src="<?php echo base_url(); ?>public/js/foundation.min.js"></script> 
-	<script> 
+	<script>
+//		files that use this footer will use Foundation framework 
 		$(document).foundation();
-	</script> 
+	</script>
+	<script type="text/javascript">
+//		google analytics tracker
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-47163924-1']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+</script> 
 
 </body>
 </html>
