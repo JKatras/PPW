7ef2c9b06777d0921aa1c8d9739740e7d9f84cea
Jeremy Katras <jkatras@fullsail.edu>
2014-01-16 20:04:40 -0500

    cleanup home controller

--- OhmieMandi/static/application/controllers/home.php
+++ OhmieMandi/static/application/controllers/home.php
@@ -3,7 +3,6 @@ class Home extends CI_controller {
 
 	public function __construct() {
 		parent::__construct();
-//		$this->load->model('usersModel');
 	}
 	
 	public function index() {
@@ -13,34 +12,28 @@ class Home extends CI_controller {
 		if(!empty($_GET["action"])){
 		
 			if($_GET["action"]=="home"){
-//				$data['regionId'] = $this->usersModel->getRegionList();
-//				$this->load->view('regionBody', $data);
+
 				$this->load->view('homeGallery');
 			}
 			if($_GET["action"]=="detail"){
-//				$result = $this->usersModel->getCharList($_GET['regionId']);
-//				$this->load->view('charByRegion', $result);
+
 				$this->load->view('detail');
 			}
 			if($_GET["action"]=="checkout"){
-//				$data['regionId'] = $this->usersModel->getRegionList();
-//				$this->load->view('regionBody', $data);
+
 				$this->load->view('checkoutForm');
 			}
 			if($_GET["action"]=="videos"){
-			//	$view->getView("views/contactForm.php");
-//				header("location: userMain.php");
+
 				$this->load->view('videos');
 			}
 			if($_GET["action"]=="contact"){
-			//	$view->getView("views/contactForm.php");
-//				header("location: userMain.php");
+
 				$this->load->view('contactForm');
 			}
 			
 		} else {
-//			$data['regionId'] = $this->usersModel->getRegionList();
-//			$this->load->view('regionBody', $data);
+
 			$this->load->view('homeGallery');
 		}
 		
