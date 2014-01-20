41c7c25f0394e68b00f01b80219b5bb9e3edd7a3
Jeremy Katras <jkatras@fullsail.edu>
2014-01-20 14:51:28 -0500

    duplicated files into 'cmd' folder (renamed from codeigniter)

---
+++ OhmieMandi/cms/application /controllers/home.php
@@ -0,0 +1,44 @@
+<?php
+class Home extends CI_controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function index() {
+		
+		$this->load->view('header');
+		
+		if(!empty($_GET["action"])){
+		
+			if($_GET["action"]=="home"){
+
+				$this->load->view('homeGallery');
+			}
+			if($_GET["action"]=="detail"){
+
+				$this->load->view('detail');
+			}
+			if($_GET["action"]=="checkout"){
+
+				$this->load->view('checkoutForm');
+			}
+			if($_GET["action"]=="videos"){
+
+				$this->load->view('videos');
+			}
+			if($_GET["action"]=="contact"){
+
+				$this->load->view('contactForm');
+			}
+			
+		} else {
+
+			$this->load->view('homeGallery');
+		}
+		
+		$this->load->view('footer');
+		
+	}
+}
+?>	
