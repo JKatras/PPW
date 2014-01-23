46d678b5d56bc861bc4af0287da56237645ac5a9
Jeremy Katras <jkatras@fullsail.edu>
2014-01-22 20:24:16 -0500

    Revert "duplicated files into 'cmd' folder (renamed from codeigniter)"
    This reverts commit 41c7c25f0394e68b00f01b80219b5bb9e3edd7a3.
    
    Conflicts:
    	OhmieMandi/.htaccess
    	OhmieMandi/OhmieMandi.esproj/user.jeremykatras.espressostorage
    	OhmieMandi/cms/.htaccess
    	OhmieMandi/cms/application /config/database.php
    	OhmieMandi/cms/application /config/development/database.php
    	OhmieMandi/cms/application /controllers/home.php
    	OhmieMandi/codeigniter/.htaccess
    	OhmieMandi/codeigniter/application/config/database.php
    	OhmieMandi/codeigniter/application/config/routes.php

---
+++ OhmieMandi/codeigniter/application/views/detail.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html> <!--[if IE 9]<html class="lt-ie10" lang="en" ><![endif]--> 
+<html class="no-js" lang="en" > 
+<head> 
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<title>OhmieMandi | Steel Hawks</title> 
+	<link rel="stylesheet" href="assets/css/normalize.css"> 
+	<link rel="stylesheet" href="assets/css/foundation.min.css">
+	<link rel="stylesheet" href="assets/css/global.css" />
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div id="wrapper">
+	<header>
+		<h1>
+			<a href="index.html">
+				<img id="logo" src="assets/images/OM_logo_purple_70x70.png" alt="OhmieMandi Logo" />
+				<img id="logo-tag" src="assets/images/OM-tag.png" alt="OhmieMandi" />
+			</a>
+		</h1>
+		<nav>
+			<ul>
+				<li class="active"><a href="index.html"><h1>Games</h1></a></li>
+				<li><a href="videos.html"><h1>Videos</h1></a></li>
+				<li><a href="contact.html"><h1>Contact</h1></a></li>
+			</ul>
+		</nav>
+	</header>	
+	<div class="main-content" id="detail">
+		<div class="row">
+			<div class="large-6 columns">		
+				<img src="" alt="" />
+				<img src="" alt="" />
+				<img src="" alt="" />
+			</div>
+			<div class="large-6 columns">
+				<h1>Steel Hawks</h1>
+				<p>Your base was attacked and now it's time for you to return the fight! Shoot your way through 30 waves of gunships, bombers, and fighters. Power up your guns for an even deadlier attack! Intense dogfights await as you strap into your cockpit and fire up your engines. Good hunting, pilot! </p>
+				<p>Requires 186MB of free space</p>
+				<p>Compatibility: Mac OSX 10.6 or higher</p>
+				<p class="price">$3.99</p>
+			</div>
+			
+		</div>
+	</div>
+	<footer>
+		<ul id="social-media">
+			<li><a href=""><img src="assets/images/fb-29x29.png" alt="Facebook Logo" /></a></li>
+			<li><a href=""><img src="assets/images/twitter-29x33.png" alt="Twitter Logo" /></a></li>
+			<li><a href=""><img src="assets/images/li-29x29.png" alt="LinkedIn Logo" /></a></li>
+		</ul>
+		<ul>
+			<li><p>&copy;OhmieMandi 2013</p></li>
+		</ul>
+	</footer>
+</div>
+	<script src="assets/js/vendor/custom.modernizr.js"></script>
+	<script src="assets/js/vendor/jquery.js" type="text/javascript"></script>
+	<script src="assets/js/foundation.min.js"></script> 
+	<script> 
+		$(document).foundation();
+	</script> 
+
+</body>
+</html>
